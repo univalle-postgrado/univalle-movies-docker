b1d804e345f7a8508623dbb0f86cdf4696650857
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-09-21 15:41:40 -0400

    Fix versiones de API

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -4,7 +4,7 @@ import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
-@Controller('categories')
+@Controller('v1/categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -4,7 +4,7 @@ import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 
-@Controller('movies')
+@Controller('v1/movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
